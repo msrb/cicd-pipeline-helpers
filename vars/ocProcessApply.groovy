2e1547750c49f80eaccaaca8ee3422ed7fb84677
Michal Srb <michal@redhat.com>
2017-04-25 09:19:50 +0200

    Try to skip deploy for given context

--- vars/ocProcessApply.groovy
+++ vars/ocProcessApply.groovy
@@ -7,6 +7,12 @@ def call(body) {
     body.delegate = config
     body()
 
+    // we don't want to deploy to rh-idev now
+    if ("test-context".equals(config.context)) {
+        echo "Skipping deploy to rh-idev..."
+        return
+    }
+
     sh "oc --context=${config.context} process ${config.processParams} -f ${config.templateFile} | oc --context=${config.context} apply -f -"
 }
 
